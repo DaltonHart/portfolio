14e8c39366cca078d3f867b8f2f4f131a3694665
Dalton Hart <DaltonVaren@gmail.com>
2018-11-19 15:56:53 -0800

    installed dependacies and file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const 
+  express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  cors = require('cors')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html' , { root : __dirname});
+})
+
+
+app.listen(process.env.PORT || 8000, ()=>{
+  console.log('Listening to port 8000');
+})
