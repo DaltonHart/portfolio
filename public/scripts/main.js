c751628d59180b96103b811903d4d0625e3b785c
Dalton Hart <DaltonVaren@gmail.com>
2018-11-20 15:11:41 -0800

    created rough landing page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('sanity');
+
+$("#js-rotating").Morphist({
+    // The entrance animation type (In).
+    animateIn: "flipInX",
+    // The exit animation type (Out). Refer to Animate.css for a list of available animations.
+    animateOut: "flipOutX",
+    // The delay between the changing of each object in milliseconds.
+    speed: 4000,
+    complete: function () {
+        
+    }
+});
